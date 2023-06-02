9d1f5064aec8a6e4471c4c7f63f017d45f0e7d48
borneygit <50075321+borneygit@users.noreply.github.com>
2023-06-02 10:40:32 +0800

    Update pidcat.rb

--- Formula/pidcat.rb
+++ Formula/pidcat.rb
@@ -1,9 +1,9 @@
 class Pidcat < Formula
   desc "A logcat colored command which displays only source entries for processes of a specific application package."
   homepage "https://github.com/borneygit/pidcat"
-  url "https://github.com/borneygit/pidcat/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "2545eb4444ff4fa8de367fe0352fe699fd7c8173e5f9c91d5505c1716c2a14a5"
-  version "0.2.0"
+  url "https://github.com/borneygit/pidcat/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "90e17d0904565d27d7e149ce3c38f51513552ba8028f6dc46f033295014f5a2e"
+  version "0.2.1"
 
   depends_on "rust" => :build
 
